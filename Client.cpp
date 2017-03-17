622701f182e0db8e4be0cd2b3723d7a2b7e96860
fimka <yafim.kazak@gmail.com>
2017-03-17 11:15:59 +0200

    Added RTT evaluation

--- Client.cpp
+++ Client.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdlib>
 #include <string>
+#include <sys/time.h>
 #include "Stream.hpp"
 #include "Connector.hpp"
 using namespace std;
@@ -19,14 +20,28 @@ int main(int argc, char** argv)
   char message = 'w';
   char ack;
   struct timeval start,end;
+  double t1,t2;
   Connector* connector = new Connector();
   Stream* stream = connector->connect(argv[2], atoi(argv[1]));
   if(stream){
+	t1=0.0; t2=0.0;
+	if(gettimeofday(&start,NULL)) {
+	  printf("time failed\n");
+	  exit(1);
+	}
+
 	stream->send(&message, sizeof(char));
 	printf("sent - %c with sizeof %d\n", message, (int) sizeof(char));
 	len = stream->receive(&ack, sizeof(char));
-	//line[len] = NULL;
+	if(gettimeofday(&end,NULL)) {
+	  printf("time failed\n");
+	  exit(1);
+	}
+	t1+=start.tv_sec+(start.tv_usec/1000000.0);
+	t2+=end.tv_sec+(end.tv_usec/1000000.0);
 	printf("received - %c\n", ack);
+	//calculate and print mean rtt
+	printf("RTT = %g ms\n",(t2-t1)/100);
 	delete stream;
   }
 
